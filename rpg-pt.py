ad75e503eeeb7f79b3e4f85bebea2da161c131a9
gustavo <gu.almeidan2007@gmail.com>
2023-02-01 07:13:05 -0300

    0.1.1

--- rpg-pt.py
+++ rpg-pt.py
@@ -13,13 +13,9 @@ armadura = (5)
 print("-------------------------------------------------\n\n                                PUFFLAND RPG\nolá grande esxplorador, você estava no meio de uma grande exploração em azaath,\numa cidade fantasma, muito temida pelos demais moradores da região, \naté que quando estava subindo em uma arvove para uma melhor visão, \nvocê sentiu um empurrão vindo das suas costas que fez você cair da arvore,\ne agora você acordou em um lugar totalmente desconhecido para você, \nvocê está em uma cela subterranea!")
 
 
-opcao = input("\na cela mais parece com um esgoto do que com uma cela para prisioneiros\n tem um cheiro forte e horrivel, você não sabe dizer o que é ")
+choice = input("\na cela mais parece com um esgoto do que com uma cela para prisioneiros\n tem um cheiro forte e horrivel, você não sabe dizer o que é ")
 
-while opcao != "sair":
-    opcao = input("")
+while choice != "sair":
+    choice = input("")
 else:
     input()
-
-
-
-
