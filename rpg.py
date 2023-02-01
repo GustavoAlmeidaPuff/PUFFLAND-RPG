5d9ef80c7539b2b2d3fed9c0dcbd724dccc54ced
gustavo <gu.almeidan2007@gmail.com>
2023-02-01 06:32:49 -0300

    ciraçao do projeto

---
+++ rpg.py
@@ -0,0 +1,12 @@
+espada = (0)
+escudo = (0)
+ouro = (20)
+armadura = (5)
+#atk normal: 5
+#def normal: 5
+#
+#atk da espada:
+#
+print("-------------------------------------------------\n\n                                PUFFLAND RPG\nolá grande esxplorador, voce estava no meio de uma grande exploração em azaath,\numa cidade fantasma, muito temida pelos demais moradores da região, \naté que quando voce estava subindo em uma arvove para uma visão melhor, \nvoce sentiu um empurrão vindo das suas costas que fez voce cair da arvore,\ne agora voce acordou em um lugar totalmente desconhecido para voce, \nvoce está em uma cela subterranea!")
+
+c1 = input("\n")
